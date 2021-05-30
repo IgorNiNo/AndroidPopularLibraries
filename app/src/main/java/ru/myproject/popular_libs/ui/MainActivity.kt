89dadfc2d25407b16340bf3ea58d3e9dd0b89ce6
IgorNiNo <vis_74@mail.ru>
2021-05-30 22:18:47 +0300

    lesson1_commit1

---
+++ app/src/main/java/ru/myproject/popular_libs/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package ru.myproject.popular_libs.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.myproject.popular_libs.databinding.ActivityMainBinding
+import ru.myproject.popular_libs.presenter.MainPresenter
+import ru.myproject.popular_libs.view.MainView
+
+class MainActivity : AppCompatActivity(), MainView {
+
+    private var vb: ActivityMainBinding? = null
+    val presenter = MainPresenter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb?.root)
+
+        vb?.btnCounter1?.setOnClickListener { presenter.counterClick1() }
+        vb?.btnCounter2?.setOnClickListener { presenter.counterClick2() }
+        vb?.btnCounter3?.setOnClickListener { presenter.counterClick3() }
+    }
+
+    override fun setButtonText1(text: String) {
+        vb?.btnCounter1?.text = text
+    }
+
+    override fun setButtonText2(text: String) {
+        vb?.btnCounter2?.text = text
+    }
+
+    override fun setButtonText3(text: String) {
+        vb?.btnCounter3?.text = text
+    }
+}
